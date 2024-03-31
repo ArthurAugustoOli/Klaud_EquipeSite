77b41368be1eb889f34965cebc7c2e83a1d62260
Arthur Augusto <157657181+ArthurAugustoOli@users.noreply.github.com>
2024-03-31 00:38:17 -0300

    Update myscripts.js

---
+++ Klaud/myscripts.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let photo = document.getElementById('addIMB');
+let file = document.getElementById('imgSelect');
+
+photo.addEventListener('click', () => {
+
+    file.click();
+
+});
+
+
+
+
+let nomeGrupo = document.getElementById('inputNomeGrupo');
+
+let lemaGrupo = document.getElementById('inputLemaDoGrupo');
